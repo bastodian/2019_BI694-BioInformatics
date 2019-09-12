4c3b37d3338970e9006eadb1e7dd8d1382fc8023
bastodian <bastodian@gmail.com>
2019-09-13 08:44:12 +1000

    Lab_03

---
+++ Lab_3/01_BLAST_Parser/ParseBlast_v4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+
+BLAST_Results_File = sys.argv[1]
+
+with open(BLAST_Results_File, 'r') as BLAST_Results:
+    Line = BLAST_Results.readline()
+    while Line:
+	if '#' not in Line:
+            LineAsList = Line.rstrip().split('\t')
+            print LineAsList
+            PercentIdentity = LineAsList[2]
+            if PercentIdentity >= float(95):
+                Query = LineAsList[0]
+                DbHit = LineAsList[1]
+                Evalue = LineAsList[-2]
+                sys.stdout.write('%s,%s,%s,%s\n' % (Query, DbHit, Evalue, PercentIdentity))
+                Line = BLAST_Results.readline()
+            else:
+                Line = BLAST_Results.readline()
+	else:
+            Line = BLAST_Results.readline()
