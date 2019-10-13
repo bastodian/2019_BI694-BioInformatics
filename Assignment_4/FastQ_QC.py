3fec25a58a65ccf2e43164b2c8370c604f0b410d
bastodian <bastodian@gmail.com>
2019-10-13 22:27:26 +1000

    Assignment 4

---
+++ Assignment_4/FastQ_QC.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from Bio Import SeqIO
+from sys import argv
+
+Fastq = argv[1]
+OutFile = argv[2]
+
+GoodSeqs = []
+for rec in SeqIO.parse(Fastq, "fastq"):
+	PhredScores = rec.letter_annotations['phred_quality']
+	if sume(PhredScores) / len(PhredScores) >= int(20):
+		GoodSeqs.append(rec)
+		
+SeqIO.write(GoodSeqs, OutFile, "fastq")
